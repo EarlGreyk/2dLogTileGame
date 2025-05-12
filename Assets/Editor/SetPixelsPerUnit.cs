6d7f3777f2d97d950fec6fd186498294f9b0cef9
EarlGreyk <heji0710234@gmail.com>
2025-05-12 22:48:15 +0900

    2025-05-12 tilemap
    타일 setpixelsper 재설정
    
    타일맵 작업 기획자가 할수있도록 기초공사.

---
+++ Assets/Editor/SetPixelsPerUnit.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SetPixelsPerUnit : MonoBehaviour
+{
+    [MenuItem("Tools/Change Pixels Per Unit")]
+    static void ChangeAllSpritesPPU()
+    {
+        string[] guids = AssetDatabase.FindAssets("t:Texture2D", new[] { "Assets/RafaelMatos/ERW-Ancient Ruins V1.9.1" });
+        int targetPPU = 32; // 원하는 PPU로 변경하세요
+
+        foreach (string guid in guids)
+        {
+            string path = AssetDatabase.GUIDToAssetPath(guid);
+            TextureImporter importer = AssetImporter.GetAtPath(path) as TextureImporter;
+
+            if (importer != null && importer.textureType == TextureImporterType.Sprite)
+            {
+                importer.spritePixelsPerUnit = targetPPU;
+                importer.SaveAndReimport();
+                Debug.Log($"Updated PPU to {targetPPU} for: {path}");
+            }
+        }
+
+        AssetDatabase.Refresh();
+    }
+}
